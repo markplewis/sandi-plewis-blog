ac0d84789cc9e9f93eca3e51615963736063ee5d
Mark Plewis <markplewis1@gmail.com>
2022-12-23 17:20:06 -0500

    Adds more detailed code comments, etc.

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -1,18 +1,19 @@
-import getSecret from "lib/getSecret";
-
 // See: https://github.com/sanity-io/next-sanity#custom-token-auth
 
-export default async function preview(req, res) {
-  // The secret can't be stored in an env variable with a NEXT_PUBLIC_ prefix, as it would make you vulnerable to leaking the token to anyone.
-  // If you don't have an custom API with authentication that can handle checking secrets, you may use https://github.com/sanity-io/sanity-studio-secrets to store the secret in your dataset.
-  const secret = await getSecret();
+// The secret can't be stored in an env variable with a NEXT_PUBLIC_ prefix, as it would make you
+// vulnerable to leaking the token to anyone. If you don't have an custom API with authentication
+// that can handle checking secrets, you may use https://github.com/sanity-io/sanity-studio-secrets
+// to store the secret in your dataset.
+const secret = process.env.SANITY_API_PREVIEW_SECRET;
+const token = process.env.SANITY_API_PREVIEW_READ_TOKEN;
 
-  // This is the most common way to check for auth, but we encourage you to use your existing auth infra to protect your token and securely transmit it to the client
+export default async function preview(req, res) {
+  // This is the most common way to check for auth, but we encourage you to use your existing auth
+  // infra to protect your token and securely transmit it to the client
   if (!req.query.secret || req.query.secret !== secret) {
     return res.status(401).json({ message: "Invalid secret" });
   }
-
-  res.setPreviewData({ token: process.env.SANITY_API_READ_TOKEN });
+  res.setPreviewData({ token });
   res.writeHead(307, { Location: "/" });
   res.end();
 }
--- pages/index.js
+++ pages/index.js
@@ -24,6 +24,59 @@ export default function IndexPage({ preview, token, data }) {
   return <DocumentsCount data={data} />;
 }
 
+// ------------------------------------- //
+
+// // This function gets called at build time on the server side ("Static Generation"). It may be
+// // called again, via a serverless function ("Incremental Static Regeneration"), if revalidation
+// // is enabled and a new request comes in (see below). See:
+// // https://nextjs.org/docs/basic-features/data-fetching/get-static-props
+// // https://nextjs.org/docs/basic-features/data-fetching/incremental-static-regeneration
+
+// export async function getStaticProps({ params }) {
+//   const post = await client.fetch(postQuery, {
+//     slug: params.slug
+//   });
+//   return {
+//     props: {
+//       data: { post }
+//     },
+//     // Return a 404 status and page if the post doesn't exist yet or no longer exists
+//     notFound: !post,
+//     // When `revalidate` is `false` (its default value) the page will be cached as built until your
+//     // next build. Otherwise, Next.js will attempt to re-generate the page when a request comes in,
+//     // once every X seconds (at most).
+//     revalidate: 10 // In seconds
+//   };
+// }
+
+// // Specify dynamic routes to pre-render pages based on data.
+// // This function gets called at build time on the server side ("Static Generation"). It may be
+// // called again, via a serverless function ("Incremental Static Regeneration"), if the requested
+// // path has not been generated yet (i.e. when new posts are published after a build). Without this
+// // mechanism in place, the site would need to be rebuilt every time a new post is published.
+// // See: https://nextjs.org/docs/basic-features/data-fetching#getstaticpaths-static-generation
+
+// export async function getStaticPaths() {
+//   // Pre-render only these paths at build time
+//   const paths = await client.fetch(
+//     `*[_type == "post" && defined(slug.current)]{ "params": { "slug": slug.current } }`
+//   );
+//   // `fallback: blocking` will server-render pages on demand if the path
+//   // wasn't statically pre-rendered (i.e. didn't exist at build time)
+//   // https://nextjs.org/docs/basic-features/data-fetching#fallback-blocking
+//   // https://nextjs.org/docs/basic-features/data-fetching#the-fallback-key-required
+//   return {
+//     paths,
+//     fallback: "blocking"
+//   };
+// }
+
+// // More information about CSR (Client-Side Rendering), SSR (Server-Side Rendering),
+// // SSG (Static-Site Generation), and ISR (Incremental Static Regeneration):
+// // https://youtu.be/f1rF9YKm1Ms
+
+// ------------------------------------- //
+
 // import Head from "next/head";
 // import Image from "next/image";
 // import { Inter } from "@next/font/google";
