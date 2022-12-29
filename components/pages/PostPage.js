14a1be1ed938aeadec77593fe3ccfcf046b55306
Mark Plewis <markplewis1@gmail.com>
2022-12-29 00:02:56 -0500

    Reimplements portable text components

--- components/pages/PostPage.js
+++ components/pages/PostPage.js
@@ -2,9 +2,24 @@ import Link from "next/link";
 import Date from "components/global/Date";
 import Layout from "components/global/Layout";
 import PageTitle from "components/global/PageTitle";
+import { PortableText } from "lib/sanity";
+
+import InternalLink from "components/portableText/InternalLink";
+import LineBreak from "components/portableText/LineBreak";
+import PostBodyImage from "components/portableText/PostBodyImage";
+
+const portableTextComponents = {
+  types: {
+    image: ({ value }) => <PostBodyImage value={value} />,
+    break: ({ value }) => <LineBreak value={value} />
+  },
+  marks: {
+    internalLink: ({ children, value }) => <InternalLink value={value}>{children}</InternalLink>
+  }
+};
 
 export function PostPage({ data }) {
-  const { title = "", description = "", date = "", categories = [], author = {} } = data;
+  const { title = "", body = "", description = "", date = "", categories = [], author = {} } = data;
   return (
     <Layout title={title} description={description}>
       <PageTitle>{title}</PageTitle>
@@ -14,6 +29,9 @@ export function PostPage({ data }) {
           {author?.name}
         </Link>
       </p>
+
+      <PortableText value={body} components={portableTextComponents} />
+
       {categories && categories.length ? (
         <div>
           <p>{categories.length > 1 ? "Categories:" : "Category:"}</p>
---
+++ components/portableText/PostBodyImage.js
@@ -0,0 +1,71 @@
+import Image from "next/legacy/image";
+import { urlFor } from "lib/sanity";
+import styles from "components/portableText/PostBodyImage.module.css";
+import { processCreditLine } from "utils/strings";
+import { rem } from "utils/units";
+import useMediaQuery from "utils/useMediaQuery";
+
+const PostBodyImage = ({ value }) => {
+  const isMedium = useMediaQuery(`(min-width: ${rem(800)})`);
+  const alignment = isMedium ? value.alignment : "center";
+  const alignmentClass = alignment ? `align-${alignment}` : "";
+  const width = value?.asset?.metadata?.dimensions?.width ?? 0;
+  const height = value?.asset?.metadata?.dimensions?.height ?? 0;
+  const creditLine = processCreditLine(value?.asset?.creditLine);
+
+  let orientation = "square";
+  if (width > height) {
+    orientation = "landscape";
+  } else if (height > width) {
+    orientation = "portrait";
+  }
+  // 9:14 aspect ratio for portrait, 3:2 for landscape
+  let imageWidth;
+  let imageHeight;
+  switch (orientation) {
+    case "square":
+      imageWidth = alignment === "center" ? 600 : 200;
+      imageHeight = alignment === "center" ? 600 : 200;
+      break;
+    case "portrait":
+      imageWidth = alignment === "center" ? 400 : 188;
+      imageHeight = alignment === "center" ? 622 : 292;
+      break;
+    case "landscape":
+    default:
+      imageWidth = alignment === "center" ? 600 : 300;
+      imageHeight = alignment === "center" ? 400 : 200;
+      break;
+  }
+  const image = value.asset ? (
+    <Image
+      src={urlFor(value.asset)
+        .width(imageWidth * 2)
+        .height(imageHeight * 2)
+        .url()}
+      width={imageWidth}
+      height={imageHeight}
+      // Media query `max-width` must match the one in `PostBodyImage.module.css`
+      sizes={`(max-width: 800px) 100vw, ${imageWidth * 2}px`}
+      layout="responsive"
+      alt={value?.alt}
+      placeholder="blur"
+      // Data URL generated here: https://png-pixel.com/
+      blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mM8UQ8AAhUBSQV8WJQAAAAASUVORK5CYII="
+    />
+  ) : null;
+
+  return (
+    <figure
+      className={`${styles.figure} ${styles[alignmentClass]}`}
+      style={{ maxWidth: rem(imageWidth) }}>
+      {image}
+      <figcaption className={styles.caption}>
+        {value.caption && <span>{value.caption}</span>}{" "}
+        {creditLine && <span dangerouslySetInnerHTML={{ __html: `Photo: ${creditLine}` }} />}
+      </figcaption>
+    </figure>
+  );
+};
+
+export default PostBodyImage;
