32dc3a547724a3d163889f8b3c1ed43a31054988
Mark Plewis <markplewis1@gmail.com>
2022-12-30 16:54:33 -0500

    Test

--- components/pages/CategoryPage.js
+++ components/pages/CategoryPage.js
@@ -6,24 +6,22 @@ import PageTitle from "components/global/PageTitle";
 // import styles from "styles/pages/contentListing.module.css";
 
 export function CategoryPage({ data }) {
-  const { title = "", posts = [] } = data;
+  const { title = "", posts = [] } = data || {};
   return (
     <Layout title={`Category: ${title}`} description={`Blog posts in category: ${title}`}>
       <PageTitle>{title}</PageTitle>
       <ul>
-        {posts.length
-          ? posts.map(post => (
-              <li key={`posts-${post?._id}-${post?.slug}`}>
-                <Link as={`/posts/${post?.slug}`} href={`/posts/[slug]`}>
-                  <h3>{post?.title}</h3>
-                  <p>
-                    <Date dateString={post?.date} />
-                  </p>
-                  <p>{post?.description}</p>
-                </Link>
-              </li>
-            ))
-          : null}
+        {posts.map(post => (
+          <li key={`posts-${post?._id}-${post?.slug}`}>
+            <Link as={`/posts/${post?.slug}`} href={`/posts/[slug]`}>
+              <h3>{post?.title}</h3>
+              <p>
+                <Date dateString={post?.date} />
+              </p>
+              <p>{post?.description}</p>
+            </Link>
+          </li>
+        ))}
       </ul>
     </Layout>
   );
