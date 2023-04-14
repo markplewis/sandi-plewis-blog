97340ab819083d9d09f84d389d854939dbd6c171
Mark Plewis <markplewis1@gmail.com>
2023-04-14 00:06:16 -0400

    Replaces RecentPost matchMedia queries with CSS media queries

--- components/homePage/AuthorBio.js
+++ components/homePage/AuthorBio.js
@@ -1,10 +1,7 @@
-import Image from "next/image";
-import { PortableText, urlFor } from "lib/sanity";
+import { PortableText } from "lib/sanity";
+import AuthorImage from "components/AuthorImage";
 import MoreLink from "components/MoreLink";
 import InternalLink from "components/portableText/InternalLink";
-import designTokens from "styles/design-tokens";
-import { imageBlurDataURL } from "utils/images";
-import useMediaQuery from "utils/useMediaQuery";
 
 import styles from "components/homePage/AuthorBio.module.css";
 
@@ -15,31 +12,29 @@ const portableTextComponents = {
 };
 
 export default function AuthorBio({ author }) {
-  const { breakpoints } = designTokens;
-  const smallAuthorImage = useMediaQuery(
-    `(min-width: ${breakpoints.w1150.value}rem) and (max-width: ${
-      breakpoints.w1280.value - 0.1
-    }rem)`
-  );
-  const authorImageSize = smallAuthorImage ? 140 : 175;
+  const authorImageUrl = author?.image;
+  const authorImageAlt = author?.image?.alt || author?.name;
+  const authorImageBlur = author?.image?.lqip;
 
   return (
     <section className={styles.authorBio}>
-      {author?.image ? (
-        <div className={styles.authorBioImage} style={{ maxWidth: `${authorImageSize}px` }}>
-          <Image
-            src={urlFor(author?.image)
-              .width(authorImageSize * 2)
-              .height(authorImageSize * 2)
-              .url()}
-            width={authorImageSize}
-            height={authorImageSize}
-            alt={author?.image?.alt || author?.name}
-            placeholder="blur"
-            blurDataURL={author?.image?.lqip || imageBlurDataURL}
-            className="responsive-image"
+      {authorImageUrl ? (
+        <>
+          <AuthorImage
+            className={`${styles.authorBioImage} ${styles.authorBioImageSmall}`}
+            url={authorImageUrl}
+            width={140}
+            alt={authorImageAlt}
+            blur={authorImageBlur}
           />
-        </div>
+          <AuthorImage
+            className={`${styles.authorBioImage} ${styles.authorBioImageLarge}`}
+            url={authorImageUrl}
+            width={175}
+            alt={authorImageAlt}
+            blur={authorImageBlur}
+          />
+        </>
       ) : null}
 
       <h2 className={styles.authorBioHeading}>{author?.name}</h2>
--- components/homePage/RecentPostList.js
+++ components/homePage/RecentPostList.js
@@ -1,8 +1,5 @@
-import Image from "next/image";
 import Link from "next/link";
-import { urlFor } from "lib/sanity";
-import { imageBlurDataURL } from "utils/images";
-import useMediaQuery from "utils/useMediaQuery";
+import RecentPostImage from "components/homePage/RecentPostImage";
 import designTokens from "styles/design-tokens";
 
 import styles from "components/homePage/RecentPostList.module.css";
@@ -12,66 +9,77 @@ const sizes = {
     width: 240,
     // Possible heights
     // 3:2 aspect ratio for landscape, 9:14 for portrait
-    landscape: 160,
-    portrait: 374
+    landscape: 160
+    // portrait: 374 (not used)
   },
   small: {
     width: 83,
-    landscape: 55,
-    portrait: 129
+    landscape: 55
+    // portrait: 129 (not used)
   },
   large: {
     width: 120,
-    landscape: 80,
-    portrait: 187
+    landscape: 80
+    // portrait: 187 (not used)
   }
 };
 
 export default function RecentPostsList({ posts }) {
   const { breakpoints } = designTokens;
-  const isNarrow = useMediaQuery(`(min-width: ${breakpoints.w480.value}rem)`);
-  const isMedium = useMediaQuery(
-    `(min-width: ${breakpoints.w520.value}rem) and (max-width: ${breakpoints.w1150.value - 0.1}rem)`
-  );
-  const isWide = useMediaQuery(`(min-width: ${breakpoints.w1280.value}rem)`);
-
-  const largePostImages = isMedium || isWide;
-  const imageSize = !isNarrow ? sizes.mobile : sizes[largePostImages ? "large" : "small"];
-  const imageWidth = imageSize.width;
-  const imageHeight = imageSize.landscape;
 
   return (
     <ul className={styles.recentPostList}>
-      {posts.map(post => (
-        <li key={`posts-${post?._id}-${post?.slug}`}>
-          <Link
-            className={styles.recentPostLink}
-            as={`/posts/${post?.slug}`}
-            href={`/posts/[slug]`}>
-            {post?.image ? (
-              <div className={styles.recentPostImage} style={{ maxWidth: `${imageWidth}px` }}>
-                <Image
-                  src={urlFor(post?.image)
-                    .width(imageWidth * 2)
-                    .height(imageHeight * 2)
-                    .url()}
-                  width={imageWidth}
-                  height={imageHeight}
-                  sizes={`(max-width: ${breakpoints.w800.value}rem) 100vw, ${imageWidth}px`}
-                  alt={post?.image?.alt}
-                  placeholder="blur"
-                  blurDataURL={post?.image?.lqip || imageBlurDataURL}
-                  className="responsive-image"
-                />
+      {posts.map(post => {
+        const imageUrl = post?.image;
+        const imageAlt = post?.image?.alt;
+        const imageBlur = post?.image?.lqip;
+        const imageBreakpoint = breakpoints.w800.value;
+
+        return (
+          <li key={`posts-${post?._id}-${post?.slug}`}>
+            <Link
+              className={styles.recentPostLink}
+              as={`/posts/${post?.slug}`}
+              href={`/posts/[slug]`}>
+              {post?.image ? (
+                <>
+                  <RecentPostImage
+                    className={`${styles.recentPostImage} ${styles.recentPostImageMobile}`}
+                    url={imageUrl}
+                    width={sizes.mobile.width}
+                    height={sizes.mobile.landscape}
+                    sizes={`(max-width: ${imageBreakpoint}rem) 100vw, ${sizes.mobile.width}px`}
+                    alt={imageAlt}
+                    blur={imageBlur}
+                  />
+                  <RecentPostImage
+                    className={`${styles.recentPostImage} ${styles.recentPostImageSmall}`}
+                    url={imageUrl}
+                    width={sizes.small.width}
+                    height={sizes.small.landscape}
+                    sizes={`(max-width: ${imageBreakpoint}rem) 100vw, ${sizes.small.width}px`}
+                    alt={imageAlt}
+                    blur={imageBlur}
+                  />
+                  <RecentPostImage
+                    className={`${styles.recentPostImage} ${styles.recentPostImageLarge}`}
+                    url={imageUrl}
+                    width={sizes.large.width}
+                    height={sizes.large.landscape}
+                    sizes={`(max-width: ${imageBreakpoint}rem) 100vw, ${sizes.large.width}px`}
+                    alt={imageAlt}
+                    blur={imageBlur}
+                  />
+                </>
+              ) : null}
+              <div className={styles.recentPostInfo}>
+                <h3 className={styles.recentPostTitle}>{post?.title || post?.name}</h3>
+                <p className={styles.recentPostDescription}>{post?.description}</p>
               </div>
-            ) : null}
-            <div className={styles.recentPostInfo}>
-              <h3 className={styles.recentPostTitle}>{post?.title || post?.name}</h3>
-              <p className={styles.recentPostDescription}>{post?.description}</p>
-            </div>
-          </Link>
-        </li>
-      ))}
+            </Link>
+          </li>
+        );
+      })}
     </ul>
   );
 }
